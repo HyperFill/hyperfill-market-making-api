08983f2a653470eb37083efb122e49af614ac01a
NorVirae <norbertmbafrank@gmail.com>
2025-08-24 21:25:21 +0100

    update dynamic port

--- app.py
+++ app.py
@@ -834,4 +834,5 @@ async def settlement_health():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = os.getenv["PORT"] | 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
